92ae74b8373c616f4a4c49c435c78719a4f383ce
김영진 <zmfjrxmfjr@gmail.com>
2019-07-01 19:13:23 +0900

    make model in community app

--- tutorial/community/models.py
+++ tutorial/community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Class for BullteinBoard
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    url = models.URLField()
+    email = models.EmailField()
+    cdate = models.DateTimeField(auto_now_add=True)
