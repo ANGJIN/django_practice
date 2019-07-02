2a4b582f049758c34dc5703bd82e193e8a6a9874
angjin <zmfjrxmfjr@gmail.com>
2019-07-02 10:24:47 +0900

    make form to process POST

--- tutorial/community/views.py
+++ tutorial/community/views.py
@@ -3,6 +3,15 @@ from community.forms import *
 
 # Create your views here.
 def write(request): #write with user request
-    form = Form()
+    #clicking form button occurs POST request
+    #request.mehthod is POST or GET
+    if request.method == 'POST':
+        #if POST, get contents of request.POST
+        form = Form(request.POST)
+        if form.is_valid(): #check for form data is valid
+            form.save() #save data in form to DB
+    else :
+        #if not POST, just print form
+        form = Form()
     #send form data to write.html template
     return render(request, 'write.html',{'form':form})
